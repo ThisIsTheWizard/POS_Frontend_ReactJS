fded7d4ff9eebbb1666e62d81dc557a28402e93b
The Wizard <thewizard3698@gmail.com>
2021-11-19 16:47:23 +0600

    Added apollo client setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { gql, useQuery } from '@apollo/client'
 import './App.css'
 
+const GET_MULTILPLE_USERS = gql`
+  query {
+    users {
+      _id
+      name
+      email
+    }
+  }
+`
+
 const App = () => {
+  const { loading, error, data } = useQuery(GET_MULTILPLE_USERS)
+  const users = () => {
+    if (loading) {
+      return 'Loading...'
+    } else if (error) {
+      return error.message
+    } else if (data) {
+      return data.users.map((d) => <div key={d._id}>{d.name}</div>)
+    }
+  }
   return (
     <div>
-      <div className="login-page">Login</div>
+      <div>{users()}</div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/offline/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJhcHBfYWRtaW4iXSwiZGF0YSI6eyJ1c2VySWQiOiJTWVNURU0iLCJzeXN0ZW0iOiJ2Mi11bC1zZXJ2aWNlLWRldi1jcmVhZGl0LXJhdGluZyJ9fQ.UdPOgl8nrWZTAvmsBHChOqbVpWpHFclN5PzV58gap1g'
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
@@ -13,4 +44,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals(console.log)
